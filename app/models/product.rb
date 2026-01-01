9507969cebb3413a0e40f6511accbff5feaaf322
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 08:32:31 +0545

    fix: add validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
     belongs_to :category
   has_many :order_items
   has_many :orders, through: :order_items
+  validates :Name, presence:true
+  validates :Description, presence:true
+  validates :price, presence:true
 end
