519fe11cd4fd265fd6db9f20e1d8b52021867aec
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 22:49:11 +0545

    fix: add Default scope in shipping details  model

--- app/models/shipping_detail.rb
+++ app/models/shipping_detail.rb
@@ -3,5 +3,7 @@ class ShippingDetail < ApplicationRecord
 validates :address, presence:true
 validates :city, presence:true
 validates :state, presence:true
-validates :shipped_at, presence:true     
+validates :shipped_at, presence:true   
+
+default_scope { order(created_at: :desc) }
 end
