6c6f3c2ffec46a5a62a8c152e913b14b1a78e0d4
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 21:19:16 +0545

    fix: add callback in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,17 @@
 class Customer < ApplicationRecord
       has_many :orders
+   before_save :before_save
+  after_save  :after_save
+  def before_save
+    puts "before_save"
+  end
+
+  def after_save
+    puts "after_save"
+  end
       validates :Address, presence:true
       validates :FirstName, presence:true
       validates :LastName, presence:true
       validates :phone, presence:true, uniqueness:true
+      
 end 
