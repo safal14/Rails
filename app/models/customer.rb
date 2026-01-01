d85f8599c145e61de488ba428122423caf921301
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 08:13:53 +0545

    fix: add validation to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
       has_many :orders
-
-end
+      validates :Address, presence:true
+      validates :FirstName, presence:true
+      validates :LastName, presence:true
+      validates :phone, presence:true, uniqueness:true
+end 
