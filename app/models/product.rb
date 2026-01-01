b13018c80ac6399ef254d0c1f337ce22056a1676
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 21:06:53 +0545

    fix: add callback function in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ApplicationRecord
     belongs_to :category
   has_many :order_items
   has_many :orders, through: :order_items
-  before_validation :before_validation
-  after_create      :after_create
+   before_validation :before_validation_
+  def before_validation_
+    puts "before_validation"
+  end
   validates :Name, presence:true
   validates :Description, presence:true
   validates :price, presence:true
