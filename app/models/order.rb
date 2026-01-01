aa2a867309f5faea194da27ac7f327ac62b461c0
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 08:30:22 +0545

    fix: add validation to order  model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
   has_one :shipping_detail
+  validates :total, presence:true
+  validates :ordered_at, presence:true
+  
 end
