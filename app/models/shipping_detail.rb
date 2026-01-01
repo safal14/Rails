4694134da7dfff257398cc89bcc00f645f00bb00
Safal Acharya <acharyasafal51@gmail.com>
2026-01-01 08:35:06 +0545

    fix: add validation to shipping_detail  model

--- app/models/shipping_detail.rb
+++ app/models/shipping_detail.rb
@@ -1,3 +1,7 @@
 class ShippingDetail < ApplicationRecord
     belongs_to :order
+validates :address, presence:true
+validates :city, presence:true
+validates :state, presence:true
+validates :shipped_at, presence:true     
 end
